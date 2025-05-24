ab4520e1f66113a9620e76ec693f212e6a2e4d87
TheBigTig <kaichun24@gmail.com>
2025-05-23 23:02:48 -0700

    test file and updated readme for myslef lmao

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# php practice message center
+# PHP Practice Message Center
+
+## Running Frontend
+
+~ cd frontend
+~ php -S localhost:3000
+
+## Running Backend
+
+~ cd backend
+~ php -S localhost:3001 router.php
---
+++ frontend/pages/test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <title>Test</title>
+    <script>
+        async function test() {
+            const response = await fetch('http://localhost:3001/routes/users?userId=1', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+
+            if (data.status === 'success') {
+                console.log('Success:', data);
+            } else {
+                console.error('Error:', data);
+            }
+        }
+    </script>
+</head>
+<body>
+    <button onclick="test()">1</button>
+</body>
