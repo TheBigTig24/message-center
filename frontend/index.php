09896681cae74afc26eec7d00e2f54a8e1bd7998
TheBigTig <kaichun24@gmail.com>
2025-05-23 23:01:01 -0700

    minor directory fixes

---
+++ frontend/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+    <title>Index</title>
+    <script src="/scripts/index.js"></script>
+</head>
+<body>
+</body>
--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    window.location.href = 'login.php';
+    window.location.href = '/pages/login.php';
 });
